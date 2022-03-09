58636529d72463451996a30d5a2a260929bd6986
fcaravaca <100384112@alumnos.uc3m.es>
2022-03-09 17:13:08 +0100

    Change uuid to str form

--- src/Health_Authority/mobileIDsRequest.py
+++ src/Health_Authority/mobileIDsRequest.py
@@ -21,7 +21,7 @@ def mobile_id_request(n, transaction_id, auth_key):
 
 
 if __name__ == "__main__":
-    transaction_id = uuid.uuid4()
+    transaction_id = str(uuid.uuid4())
     auth_key_ID_provider = "key"
 
-    mobile_id_request(6, transaction_id.int, auth_key_ID_provider)
+    mobile_id_request(6, transaction_id, auth_key_ID_provider)
