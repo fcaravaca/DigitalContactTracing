5b22806a07a7cfa4dbdb80c8db5e25de7a8b9592
fcaravaca <100384112@alumnos.uc3m.es>
2022-06-15 12:27:44 +0200

    Added the addition of keys and the removal of them At build time docker

--- src/ITP_Authority/start_itpa.bat
+++ src/ITP_Authority/start_itpa.bat
@@ -1,2 +1,12 @@
+copy ..\..\DevelopmentTestKeys\HA_public.pem .
+copy ..\..\DevelopmentTestKeys\LP1_public.pem .
+copy ..\..\DevelopmentTestKeys\LP2_public.pem .
+copy ..\..\DevelopmentTestKeys\LP3_public.pem .
+copy ..\..\DevelopmentTestKeys\ITPA.pem .
 docker-compose -f docker-compose.yml -p itpa-1 up --force-recreate --build -d
 docker image prune -f
+del HA_public.pem
+del LP1_public.pem
+del LP2_public.pem
+del LP3_public.pem
+del ITPA.pem
--- src/Location_Provider/start_lps.bat
+++ src/Location_Provider/start_lps.bat
@@ -1,3 +1,5 @@
+copy ..\..\DevelopmentTestKeys\HA_public.pem .
+copy ..\..\DevelopmentTestKeys\ITPA_public.pem .
 copy ..\..\DevelopmentTestKeys\LP1.pem private.pem
 docker-compose -f docker-compose.yml -f docker-compose.lp-1.yml -p lp-1 up --force-recreate --build -d
 copy ..\..\DevelopmentTestKeys\LP2.pem private.pem
@@ -5,4 +7,6 @@ docker-compose -f docker-compose.yml -f docker-compose.lp-2.yml -p lp-2 up --for
 copy ..\..\DevelopmentTestKeys\LP3.pem private.pem
 docker-compose -f docker-compose.yml -f docker-compose.lp-3.yml -p lp-3 up --force-recreate --build -d
 del private.pem
+del HA_public.pem
+del ITPA_public.pem
 docker image prune -f
